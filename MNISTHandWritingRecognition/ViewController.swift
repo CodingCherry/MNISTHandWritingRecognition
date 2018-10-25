2e225e9f22bb0ece5ba7f6075dabcfe0b99c8248
CodingCherry <cherry.alexeeva@gmail.com>
2018-10-25 16:29:42 +0700

    Initial Commit

---
+++ MNISTHandWritingRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MNISTHandWritingRecognition
+//
+//  Created by Alexeeva Ekaterina on 10/25/18.
+//  Copyright © 2018 Alexeeva Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
